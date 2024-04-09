efb1fb890d59cce9bfa6e77bae00f847a1abf037
FlorRP <157651432+FlorRP@users.noreply.github.com>
2024-04-09 18:30:45 -0300

    Update validacion.js
    Agregué variables

--- validacion.js
+++ validacion.js
@@ -1,1 +1,7 @@
 //Haz tú validación en javascript acá
+var nombre= document.getElementById(nombreUsuario).value;
+var email= document.getElementById(mailUsuario).value;
+var asunto= document.getElementById(asunto).value;
+var mensaje= document.getelementById(mensagem).value;
+const sintaxMail= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const caracteresPermitidos= /^[A-Za-z0-9\s]+$/;
