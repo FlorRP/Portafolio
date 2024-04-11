ae63f27e76bb0ed5a9ff4e010c06408b14900bee
Flora <mflorenciaroldanp@gmail.com>
2024-04-10 21:27:04 -0300

    Agregué JS, HTML-en y arreglé HTML

--- validacion.js
+++ validacion.js
@@ -1,7 +1,55 @@
 //Haz tú validación en javascript acá
-var nombre= document.getElementById(nombreUsuario).value;
-var email= document.getElementById(mailUsuario).value;
-var asunto= document.getElementById(asunto).value;
-var mensaje= document.getelementById(mensagem).value;
+let maximoCaracteres = 50;
 const sintaxMail= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const caracteresPermitidos= /^[A-Za-z0-9\s]+$/;
+
+function validarNombre(){
+    let nombre = document.getElementById('nombreUsuario').value;
+    if (nombre.length>maximoCaracteres){
+        alert ('Ha alcanzado el máximo de caracteres permitidos');
+        return false;
+    }if (!caracteresPermitidos.test(`${nombre}`)){
+            alert ('No se permiten caracteres especiales');
+            return false;
+        } else{
+            return true;
+        }
+}
+
+function validarEmail(){
+    let email= document.getElementById('mailUsuario').value;
+    if (!sintaxMail.test(email)){
+        return true;
+    } else{
+        alert ('Asegurese de haber escrito el e-mail correctamente');
+        return false;
+    }
+}
+
+function validarAsunto(){
+    let asunto = document.getElementById('asunto').value;
+    if (asunto.length>maximoCaracteres){
+        alert ('Ha alcanzado el máximo de caracteres permitidos');
+        return false;
+    } if (!caracteresPermitidos.test(`${nombre}`)){
+        alert ('No se permiten caracteres especiales');
+        return false;
+    } else{
+        return true;
+    }
+        
+}
+
+function validarMensaje(){
+    let mensaje = document.getelementById('mensagem').value;
+    let maximoCaracteres2 = 300;
+    if (mensaje.length>maximoCaracteres2){
+        alert ('Ha alcanzado el máximo de caracteres permitidos');
+        return false;} else{
+            return true;
+        }
+}
+
+function validarFormulario(){
+    return function validarNombre() && function validarEmail() && function validarAsunto() && function validarMensaje();
+}
