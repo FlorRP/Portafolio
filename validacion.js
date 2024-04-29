89c4ac8232614bc69463ff1e917234ce9146ce41
Flora <mflorenciaroldanp@gmail.com>
2024-04-29 00:03:01 -0300

    Cambios en el estilo, HTML y JS

--- validacion.js
+++ validacion.js
@@ -1,55 +1,91 @@
 //Haz tú validación en javascript acá
-let maximoCaracteres = 50;
+const camposFormulario = document.querySelectorAll("[required]");
 const sintaxMail= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const caracteresPermitidos= /^[A-Za-z0-9\s]+$/;
+const validacionFormulario = document.querySelector("[data-formulario]");
+let maximoCaracteres = 50;
 
-function validarNombre(){
-    let nombre = document.getElementById('nombreUsuario').value;
-    if (nombre.length>maximoCaracteres){
-        alert ('Ha alcanzado el máximo de caracteres permitidos');
-        return false;
-    }if (!caracteresPermitidos.test(`${nombre}`)){
-            alert ('No se permiten caracteres especiales');
-            return false;
+validacionFormulario.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const listaRespuestas={
+        nombre:e.target.elements["nombre"].value,
+        email:e.target.elements["email"].value,
+        asunto:e.target.elements["asunto"].value,
+        mensaje:e.target.elements["mensaje"].value,
+    };
+    if(validacionFormulario.checkValidity()){
+        enviarFormulario();
+        localStorage.setItem("registro", JSON.stringify(listaRespuestas))
+        alert("El envio del formulario ha sido exitoso");
+    }
+    
+})
+
+camposFormulario.forEach((campo)=>{
+    
+    campo.addEventListener("invalid", evento => evento.preventDefault());
+});
+
+function verificarCampo(campo){
+    let mensajes= "";
+    campo.setCustomValidity("");
+    if(campo.name == "nombre" && campo.value.length <=50 && campo.value != ""){
+        validarNombre(campo);
+    }
+    
+}
+
+function validarNombre(campo){
+    const nombre= campo.value;
+    if (nombre.length > maximoCaracteres){
+        campo.setCustomValidity("El nombre tiene más caracteres de los permitidos, por favor ingrese un nombre válido");
+    }else{
+        if (nombre == ""){
+            campo.setCustomValidity("Por favor, complete con su nombre");
+                
         } else{
-            return true;
+            console.log("Nombre válido");
         }
+    }
 }
 
-function validarEmail(){
-    let email= document.getElementById('mailUsuario').value;
+function validarEmail(campo){
+    const email= campo.value;
     if (!sintaxMail.test(email)){
-        return true;
+        campo.setCustomValidity("El e-mail no es válido");
     } else{
-        alert ('Asegurese de haber escrito el e-mail correctamente');
-        return false;
+        if(email == ""){
+            campo.setCustomValidity("Por favor, complete el campo de e-mail");
+        }else{
+            console.log ('E-mail válido');
+        }
     }
 }
 
-function validarAsunto(){
-    let asunto = document.getElementById('asunto').value;
+function validarAsunto(campo){
+    const asunto = campo.value;
     if (asunto.length>maximoCaracteres){
-        alert ('Ha alcanzado el máximo de caracteres permitidos');
-        return false;
-    } if (!caracteresPermitidos.test(`${nombre}`)){
-        alert ('No se permiten caracteres especiales');
-        return false;
+        campo.setCustomValidity ('Ha alcanzado el máximo de caracteres permitidos');
     } else{
-        return true;
-    }
+      if (asunto == ""){
+            campo.setCustomValidity ('Por favor, complete el campo de asunto');
+        } else{
+            console.log ('Asunto válido');
+        }  
+    } 
         
 }
 
-function validarMensaje(){
-    let mensaje = document.getelementById('mensagem').value;
-    let maximoCaracteres2 = 300;
-    if (mensaje.length>maximoCaracteres2){
-        alert ('Ha alcanzado el máximo de caracteres permitidos');
-        return false;} else{
-            return true;
+function validarMensaje(campo){
+    const mensaje = campo.value;
+    let maximoCaracteres = 300;
+    if (mensaje.length>maximoCaracteres){
+        campo.setCustomValidity ('Ha alcanzado el máximo de caracteres permitidos');
+    } else{
+        if(mensaje == ""){
+            campo.setCustomValidity ('Por favor, complete el campo de asunto');
+        } else{
+            console.log ('Asunto válido');  
         }
-}
-
-function validarFormulario(){
-    return function validarNombre() && function validarEmail() && function validarAsunto() && function validarMensaje();
+        
+    }
 }
